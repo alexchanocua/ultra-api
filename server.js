ee2b67e86907c7855b915c50206fda291db84382
Alejandro Chanocua <73016752+alexchanocua@users.noreply.github.com>
2021-08-09 22:32:45 -0700

    added root route

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ const app = express();
 app.use(express.json());
 app.use(cors);
 
+app.get('/', (req,res) => {res.send("it is working")});
+
 app.listen(process.env.PORT || 3000, () => {
     console.log(`app is running on port ${process.env.PORT}...`);
 });
