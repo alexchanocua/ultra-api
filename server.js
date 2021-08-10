72da878a505bc52f4ac100c0d17f2670ec84d1df
Alejandro Chanocua <73016752+alexchanocua@users.noreply.github.com>
2021-08-09 22:05:24 -0700

    initial files

---
+++ server.js
@@ -0,0 +1,30 @@
+// packages
+const express = require('express');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+const knex = require('knex');
+// controllers
+const signin = require('./controllers/signin');
+
+// const db = knex({
+//     client: 'pg',
+//     connection: {
+//       connectionString: process.env.DATABASE_URL, // heroku PSQL
+//       ssl: {
+//         rejectUnauthorized: false
+//       }
+//     }
+//   });
+
+// creating express server
+const app = express();
+
+// middle-ware for parsing JSON data from front-end
+app.use(express.json());
+app.use(cors);
+
+app.listen( 3000, () => {
+    console.log('app is running on port 3000...');
+});
+
+// app.post('/signin', signin.handleSignin(db, bcrypt) );
